760288a7b36685efd5f41762d4c45a200233a646
harsha0571 <harsha90571@gmail.com>
2021-09-05 21:53:33 +0530

    3.3 added

--- server.js
+++ server.js
@@ -24,14 +24,28 @@ let persons = [
     }
 ]
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
+
 app.get('/info', (req, res) => {
     res.send(`<div> PhoneBook has ${persons.length} entries 
     <br>
     as of ${Date()}</div>`)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    }
+    else {
+        res.status(400).json(`person with id=${id} doesn't exist`)
+    }
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
