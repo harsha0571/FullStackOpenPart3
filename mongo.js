1b9af68d25b521a6173109c1e7d3992e57971400
harsha0571 <harsha90571@gmail.com>
2021-09-06 19:25:03 +0530

    3.13 and 3.14 added

--- mongo.js
+++ mongo.js
@@ -1,34 +1,15 @@
-const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
-}
 
-const password = process.argv[2]
 
-const url = `mongodb+srv://harsha571:${password}@cluster0.6zkop.mongodb.net/fsoPhoneBook?retryWrites=true&w=majority`
 
-mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+
+
+person.save().then(result => {
+    console.log('person added!')
+    mongoose.connection.close()
 })
 
-const Persons = mongoose.model('Persons', personSchema)
-
-if (process.argv.length === 5) {
-    const person = new Persons({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
-
-    person.save().then(result => {
-        console.log('person added!')
-        mongoose.connection.close()
-    })
-}
 console.log("phonebook:")
 Persons.find({}).then(result => {
     result.forEach(person => {
--- server.js
+++ server.js
@@ -3,82 +3,94 @@ const cors = require('cors')
 var morgan = require('morgan')
 const app = express()
 app.use(express.json())
+require('dotenv').config()
 app.use(cors())
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
+// morgan.token('body', (req, res) => JSON.stringify(req.body));
+// app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 app.use(express.static('build'))
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+const Persons = require('./models/person.model')
+
+app.post('/api/persons', (req, res) => {
+
+    const body = req.body
+
+    if (body.name === undefined || body.number === undefined) {
+        return response.status(400).json({ error: 'number or name is missing' })
     }
-]
+
+    const person = new Persons({
+        name: body.name,
+        number: body.number
+
+    })
+
+    person.save().then(newPerson => {
+        res.json(newPerson)
+    })
+})
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+
+    Persons.find({})
+        .then(result => {
+            res.json(result)
+        })
+
 })
 
-app.get('/info', (req, res) => {
-    res.send(`<div> PhoneBook has ${persons.length} entries 
-    <br>
-    as of ${Date()}</div>`)
-})
+// app.get('/info', (req, res) => {
+//     res.send(`<div> PhoneBook has ${persons.length} entries 
+//     <br>
+//     as of ${Date()}</div>`)
+// })
+
+// app.get('/api/persons/:id', (req, res) => {
+//     const id = Number(req.params.id)
+//     const person = persons.find(person => person.id === id)
+//     if (person) {
+//         res.json(person)
+//     }
+//     else {
+//         res.status(400).json(`person with id=${id} doesn't exist`)
+//     }
+
+// })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    Persons.findById(req.params.id).then(person => {
         res.json(person)
-    }
-    else {
-        res.status(400).json(`person with id=${id} doesn't exist`)
-    }
-
+    })
 })
+
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
 })
 
-app.post('/api/persons', (req, res) => {
-    const person = req.body
-    const name = person.name
-    // const maxId = persons.length > 0
-    //     ? Math.max(...persons.map(n => n.id))
-    //     : 0
-    const flag = (persons.some(p => p.name === name))
-    if (person.name && person.number && !flag) {
-        const maxId = Math.floor(Math.random() * 100000000)
-        person.id = maxId + 1
-        persons = persons.concat(person)
-        res.json(person)
-    }
-    else if (flag) {
-        res.json("name must be unique")
-    }
-    else {
-        res.status(400).json("error :name and number must be provided")
-    }
+// app.post('/api/persons', (req, res) => {
+//     const person = req.body
+//     const name = person.name
+//     // const maxId = persons.length > 0
+//     //     ? Math.max(...persons.map(n => n.id))
+//     //     : 0
+//     const flag = (persons.some(p => p.name === name))
+//     if (person.name && person.number && !flag) {
+//         const maxId = Math.floor(Math.random() * 100000000)
+//         person.id = maxId + 1
+//         persons = persons.concat(person)
+//         res.json(person)
+//     }
+//     else if (flag) {
+//         res.json("name must be unique")
+//     }
+//     else {
+//         res.status(400).json("error :name and number must be provided")
+//     }
 
 
-})
+// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
